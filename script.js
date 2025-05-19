e4524fb82807ac67dfd10ec90459a8c4f7966ebc
Samuel Basson <samuelbasson06@gmail.com>
2025-05-19 12:29:45 +0200

    pain

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 function calculateTip(){
-    let tipFactor=1;
-    let bill;
-    let ifGenerous=false;
-    let ifAngry=false;
+  let bill=parseFloat(document.querySelector('#price').value);
+  let iftip=parseFloat(document.querySelector('#tip').value);
+  let food=parseFloat(document.querySelector('#food').value);
+  let service=parseFloat(document.querySelector('#service').value);
+  let accuracy=parseFloat(document.querySelector('#accuracy').value);
+  let speed=parseFloat(document.querySelector('#speed').value);
+  let cleanliness=parseFloat(document.querySelector('#cleanliness').value);
+  let makeathome=1-parseFloat(document.querySelector('#makeathome').value);
+  let attractiveness=parseFloat(document.querySelector('#attractiveness').value);
+  let instagrammability=parseFloat(document.querySelector('#instagrammability').value);
+  let rich=parseFloat(document.querySelector('#rich').value);
+  let impress=parseFloat(document.querySelector('#impress').value);
+  let alignment=parseFloat(document.querySelector('#alignment').value);
+  let prime=parseFloat(document.querySelector('#prime').value);
+  let cool=parseFloat(document.querySelector('#cool').value);
+  let generousity=parseFloat(document.querySelector('#generousity').value)
+  ;
+
+  return bill*iftip*food*service*accuracy*speed*cleanliness*makeathome*attractiveness*instagrammability*rich*impress*alignment*prime*cool*generousity
 }
 
-const slider = document.getElementById("slider");
-const output = document.getElementById("sliderValue");
+const total = document.querySelector('#total');
 
-slider.addEventListener("input", () => {
-  output.textContent = parseFloat(slider.value).toFixed(2);
-});
+total.innerHTML = calculateTip();
